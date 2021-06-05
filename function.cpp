12551b367b573867f2135782b218a61825551367
magendror <magendror@gmail.com>
2021-06-05 11:39:12 +0300

    unite

--- function.cpp
+++ function.cpp
@@ -1,6 +1,36 @@
 #include "library2.h"
 #include "dealership.h"
 
+//Insert Static Functions Here//
+static Agency* getRoot(Agency* current){
+    while(current->father!=NULL){
+        current=current->father;
+    }
+    return current;
+}
+
+static void clearTypeTree(TypeNode* current){
+    if (current==NULL){
+        return;
+    }
+    clearTypeTree(current->left);
+    clearTypeTree(current->right);
+    delete current->key;
+    delete current;
+}
+
+static void clearSalesTree(SalesNode* current){
+    if (current==NULL){
+        return;
+    }
+    clearSalesTree(current->left);
+    clearSalesTree(current->right);
+    delete current->key;
+    delete current;
+}
+//End of Static Functions//
+
+//Insert Main Functions Here//
 void *Init(){
     Dealership* ds= new Dealership();
     return ds;
@@ -24,8 +54,29 @@ StatusType AddAgency(void *DS){
 
 StatusType SellCar(void *DS, int agencyID, int typeID, int k);
 
-StatusType UniteAgencies(void *DS, int agencyID1, int agencyID2);
+StatusType UniteAgencies(void *DS, int agencyID1, int agencyID2){
+    if(DS==NULL||agencyID1<0||agencyID2<0){
+        return INVALID_INPUT;
+    }
+    Dealership* DS_convert = (Dealership*)DS;
+    if(agencyID1>=DS_convert->num_of_agencies||agencyID2>=DS_convert->num_of_agencies){
+        return FAILURE;
+    }
+    if(agencyID1==agencyID2){
+        return SUCCESS;
+    }
+    Agency* root1= getRoot(DS_convert->agencies[agencyID1]);
+    Agency* root2= getRoot(DS_convert->agencies[agencyID2]);
+    if(root2->num_of_agency>root1->num_of_agency){
+        root1->father=root2;
+        sales_temp=mergeTrees();
+    }
+    else{
+        root2->father=root1;
+    }
+}
 
 StatusType GetIthSoldType(void *DS, int agencyID, int i, int* res);
 
 void Quit(void** DS);
+//End of Main Functions//
